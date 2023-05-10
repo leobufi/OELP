b00227f21c560f6d82c9e38371a3a7c2392fe70d
Léo Bufi <bufi.leo@gmail.com>
2023-05-10 12:35:01 +0200

    photos mvc ok

---
+++ db/migrate/20230510100444_create_photos.rb
@@ -0,0 +1,9 @@
+class CreatePhotos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :photos do |t|
+      t.string :photo
+      t.string :side
+      t.timestamps
+    end
+  end
+end
