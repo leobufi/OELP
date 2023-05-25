bef55d1c2d8450d3a476da8f10fcb0aa113d6ff1
Léo Bufi <bufi.leo@gmail.com>
2023-05-25 17:15:45 +0200

    postgresql installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_111703) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_25_150220) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
